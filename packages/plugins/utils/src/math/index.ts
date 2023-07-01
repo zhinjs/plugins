bef8ec6d1a94c0f5fd4d059b9a297330a8b79cf1
凉菜 <admin@liucl.cn>
2023-07-01 22:34:40 +0800

    fix: utils、qa、oneBot

--- packages/plugins/utils/src/math/index.ts
+++ packages/plugins/utils/src/math/index.ts
@@ -2,69 +2,69 @@ import {Context} from "zhin";
 import * as random from './random'
 export const name='math'
 export function install(ctx:Context) {
-    const p=ctx.command('utils/math')
+    ctx.command('utils/math')
         .desc('数学计算工具')
-    p.command('math.max <...nums:number>')
+    ctx.command('utils/math.max <...nums:number>')
         .desc('返回nums中的最大值')
         .action((_,num)=>{
             return Math.max(...num).toString()
         })
-    p.command('math.min <...nums:number>')
+    ctx.command('utils/math.min <...nums:number>')
         .desc('返回nums中的最小值')
         .action((_,num)=>{
             return Math.min(...num).toString()
         })
-    p.command('math.sum <...nums:number>')
+    ctx.command('utils/math.sum <...nums:number>')
         .desc('返回nums相加的和')
         .action((_,num)=>{
             return num.reduce((a,b)=>a+b,0).toString()
         })
-    p.command('math.round <num:number>')
+    ctx.command('utils/math.round <num:number>')
         .desc('返回num四舍五入后最接近的整数')
         .action((_,num)=>{
             return Math.round(num).toString()
         })
-    p.command('math.exp <num:number>')
+    ctx.command('utils/math.exp <num:number>')
         .desc('返回num的e次幂')
         .action((_,num)=>{
             return Math.exp(num).toString()
         })
-    p.command('math.log <num:number>')
+    ctx.command('utils/math.log <num:number>')
         .desc('返回num的自然对数')
         .action((_,num)=>{
             return Math.log(num).toString()
         })
-    p.command('math.sqrt <num:number>')
+    ctx.command('utils/math.sqrt <num:number>')
         .desc('返回num的平方根')
         .action((_,num)=>{
             return Math.sqrt(num).toString()
         })
-    p.command('math.pow <num:number> [exponent:number]')
+    ctx.command('utils/math.pow <num:number> [exponent:number]')
         .desc('返回num的exponent次幂')
         .action((_,num,exponent)=>{
             return Math.pow(num,exponent).toString()
         })
-    p.command('math.floor <num:number>')
+    ctx.command('utils/math.floor <num:number>')
         .desc('返回<=num的最大整数')
         .action((_,num)=>{
             return Math.floor(num).toString()
         })
-    p.command('math.ceil <num:number>')
+    ctx.command('utils/math.ceil <num:number>')
         .desc('返回>=num的最大整数')
         .action((_,num)=>{
             return Math.ceil(num).toString()
         })
-    p.command('math.cos <num:number>')
+    ctx.command('utils/math.cos <num:number>')
         .desc('返回num的余弦值')
         .action((_,num)=>{
             return Math.cos(num).toString()
         })
-    p.command('math.sin <num:number>')
+    ctx.command('utils/math.sin <num:number>')
         .desc('返回num的正弦值')
         .action((_,num)=>{
             return Math.sin(num).toString()
         })
-    p.command('math.tan <num:number>')
+    ctx.command('utils/math.tan <num:number>')
         .desc('返回num的正切值')
         .action((_,num)=>{
             return Math.tan(num).toString()
